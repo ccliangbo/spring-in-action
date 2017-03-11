ad28a0a8351d72ec6604f8a68f2af1d2661c4268
liangbo <liangbo@xiaomi.com>
2017-03-11 16:56:40 +0800

    增加表单验证

--- src/main/java/spittr/web/SpitterController.java
+++ src/main/java/spittr/web/SpitterController.java
@@ -1,8 +1,11 @@
 package spittr.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,7 +31,10 @@ public class SpitterController {
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String processRegisteration(Spitter spitter) {
+	public String processRegisteration(@Valid Spitter spitter, BindingResult errors) {
+		if (errors.hasErrors()) {
+			return "registerForm";
+		}
 		spitterRepository.save(spitter);
 		return "redirect:/spitter/" + spitter.getUsername();
 	}
