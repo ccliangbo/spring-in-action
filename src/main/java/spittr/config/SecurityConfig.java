de0ca44c54ce93442c8e0ff2645025c59b955ce4
liangbo <liangbo@xiaomi.com>
2017-03-25 11:21:20 +0800

    增加自定义登陆页面

--- src/main/java/spittr/config/SecurityConfig.java
+++ src/main/java/spittr/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().and().authorizeRequests().regexMatchers("/spittles").authenticated().anyRequest().permitAll();
+		http.formLogin().loginPage("/login").permitAll().and().authorizeRequests().regexMatchers("/spittles")
+				.authenticated().anyRequest().permitAll();
 	}
 }
